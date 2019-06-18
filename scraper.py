7817f453f40bc2c1c00f5f1c7f03032e94b8e897
hugobarbieux <43614788+hugobarbieux@users.noreply.github.com>
2019-06-18 11:38:05 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ html = scraperwiki.scrape("https://www.compare-school-performance.service.gov.uk
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
 # Store the matched links in "matchedlinks"
-matchedlinks = root.cssselect("tbody[class='comparison-table table-collapsible even is-stickyTableHeaderActive']")
+matchedlinks = root.cssselect("tbody")
 # Print that
 print(matchedlinks)
 #
